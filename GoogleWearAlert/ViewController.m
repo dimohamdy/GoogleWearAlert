2c37a91014a5407ef134245d535f08aed5904843
Ahmed Hamdy <dimo.hamdy@gmail.com>
2014-09-06 22:22:12 +0300

    Example
    add Example for Alert Type and Position Type

--- GoogleWearAlert/ViewController.m
+++ GoogleWearAlert/ViewController.m
@@ -23,6 +23,15 @@
 -(void)viewDidAppear:(BOOL)animated{
 
     [[GoogleWearAlertObjc getInstance]prepareNotificationToBeShown:[[GoogleWearAlertViewObjc alloc]initWithTitle:@"Message" andImage:nil andWithType:Message andWithDuration:2.5 inViewController:self atPostion:Bottom canBeDismissedByUser:NO]];
+    
+
+        
+        [[GoogleWearAlertObjc getInstance]prepareNotificationToBeShown:[[GoogleWearAlertViewObjc alloc]initWithTitle:@"Success" andImage:nil andWithType:Success andWithDuration:2.5 inViewController:self atPostion:Top canBeDismissedByUser:NO]];
+    
+        [[GoogleWearAlertObjc getInstance]prepareNotificationToBeShown:[[GoogleWearAlertViewObjc alloc]initWithTitle:@"Error" andImage:nil andWithType:Error andWithDuration:2.5 inViewController:self atPostion:Top canBeDismissedByUser:NO]];
+    
+    
+        [[GoogleWearAlertObjc getInstance]prepareNotificationToBeShown:[[GoogleWearAlertViewObjc alloc]initWithTitle:@"Warning" andImage:nil andWithType:Message andWithDuration:2.5 inViewController:self atPostion:Center canBeDismissedByUser:NO]];
 
 }
 - (void)didReceiveMemoryWarning {
